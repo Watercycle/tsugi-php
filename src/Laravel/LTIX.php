92e009fc124ae531cc85c22c8847a9191e1044e7
Davo <dbhynds@gmail.com>
2018-10-18 12:00:41 -0500

    DEV-1146 merge server and get request vars

--- src/Laravel/LTIX.php
+++ src/Laravel/LTIX.php
@@ -10,11 +10,12 @@ use Illuminate\Http\Request;
 class LTIX extends \Tsugi\Core\LTIX {
 
     public static function laravelSetup(Request $request, $needed=LTIX::ALL) {
-         $launch = self::requireDataOverride(LTIX::ALL,
+        $_SERVER = array_merge($_SERVER, $request->server());
+        $launch = self::requireDataOverride(LTIX::ALL,
             null, /* pdox - default */
             $request->session(),
             null, /* current_url - default */
-            null /* request_data - default */
+            $request->all()
         );
         return $launch;
     }
